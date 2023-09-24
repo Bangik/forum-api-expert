d2617b569ee90318085c62c54bef86ea88ce7466
Achmad Dinofaldi Firmansyah <47439247+Bangik@users.noreply.github.com>
2023-09-25 00:24:46 +0700

    like use case

---
+++ src/Applications/use_case/likes/_test/AddLikeUseCase.test.js
@@ -0,0 +1,92 @@
+const CommentRepository = require('../../../../Domains/comments/CommentRepository');
+const ThreadRepository = require('../../../../Domains/threads/ThreadRepository');
+const LikeRepository = require('../../../../Domains/likes/LikeRepository');
+const AddLikeUseCase = require('../AddLikeUseCase');
+
+describe('AddLikeUseCase', () => {
+  it('should orchestrating the add like action correctly', async () => {
+    // Arrange
+    const threadId = 'thread-123';
+    const commentId = 'comment-123';
+    const credentialId = 'user-123';
+
+    const mockThreadRepository = new ThreadRepository();
+    const mockCommentRepository = new CommentRepository();
+    const mockLikeRepository = new LikeRepository();
+
+    mockThreadRepository.verifyAvailableThread = jest.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockCommentRepository.verifyAvailableComment = jest.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockCommentRepository.verifyAvailableCommentInThread = jest.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockLikeRepository.verifyLike = jest.fn()
+      .mockImplementation(() => Promise.resolve(false));
+    mockLikeRepository.addLike = jest.fn()
+      .mockImplementation(() => Promise.resolve());
+
+    const addLikeUseCase = new AddLikeUseCase({
+      likeRepository: mockLikeRepository,
+      commentRepository: mockCommentRepository,
+      threadRepository: mockThreadRepository,
+    });
+
+    // Action
+    await addLikeUseCase.execute(threadId, commentId, credentialId);
+
+    // Assert
+    expect(mockThreadRepository.verifyAvailableThread)
+      .toBeCalledWith(threadId);
+    expect(mockCommentRepository.verifyAvailableComment)
+      .toBeCalledWith(commentId);
+    expect(mockCommentRepository.verifyAvailableCommentInThread)
+      .toBeCalledWith(commentId, threadId);
+    expect(mockLikeRepository.verifyLike)
+      .toBeCalledWith(commentId, credentialId);
+    expect(mockLikeRepository.addLike)
+      .toBeCalledWith(commentId, credentialId);
+  });
+
+  it('should orchestrating the update like action correctly', async () => {
+    // Arrange
+    const threadId = 'thread-123';
+    const commentId = 'comment-123';
+    const credentialId = 'user-123';
+
+    const mockThreadRepository = new ThreadRepository();
+    const mockCommentRepository = new CommentRepository();
+    const mockLikeRepository = new LikeRepository();
+
+    mockThreadRepository.verifyAvailableThread = jest.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockCommentRepository.verifyAvailableComment = jest.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockCommentRepository.verifyAvailableCommentInThread = jest.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockLikeRepository.verifyLike = jest.fn()
+      .mockImplementation(() => Promise.resolve(true));
+    mockLikeRepository.updateLike = jest.fn()
+      .mockImplementation(() => Promise.resolve());
+
+    const addLikeUseCase = new AddLikeUseCase({
+      likeRepository: mockLikeRepository,
+      commentRepository: mockCommentRepository,
+      threadRepository: mockThreadRepository,
+    });
+
+    // Action
+    await addLikeUseCase.execute(threadId, commentId, credentialId);
+
+    // Assert
+    expect(mockThreadRepository.verifyAvailableThread)
+      .toBeCalledWith(threadId);
+    expect(mockCommentRepository.verifyAvailableComment)
+      .toBeCalledWith(commentId);
+    expect(mockCommentRepository.verifyAvailableCommentInThread)
+      .toBeCalledWith(commentId, threadId);
+    expect(mockLikeRepository.verifyLike)
+      .toBeCalledWith(commentId, credentialId);
+    expect(mockLikeRepository.updateLike)
+      .toBeCalledWith(commentId, credentialId);
+  });
+});
